4fe7d67e09e7435fc4d95ffefb13ffb47da6a67b
Cleiton Feitosa <cleiton.costa@outlook.com.br>
2021-03-19 09:23:58 -0300

    Verificador de digitos do CPF

---
+++ CPF.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    class cad_pess_fis
+{
+        public int calcdig1(int CPF)
+    {
+        int[] numeroscpf = new int[9];
+        long restante;
+        int somanumeros = 0, digito1;
+   
+
+        restante = CPF;
+        for (int i = 1; i <= 9; i++)
+        {
+            numeroscpf[i - 1] = Convert.ToInt32(restante % 10);
+            restante = Convert.ToInt32(restante / 10);
+            somanumeros = somanumeros + numeroscpf[i - 1] * (i + 1);
+
+        }
+
+        digito1 = 11 - (somanumeros % 11);
+
+        if (digito1 == 2)
+        {
+            digito1 = 2;
+        }
+        else if (digito1 >= 10)
+        {
+            digito1 = 0;
+        }
+        else
+        {
+            Console.WriteLine("erro no digito");
+        }
+
+        return (digito1);
+    }
+
+    public int calcdig2(long CPF_dig1)
+    {
+
+        int[] numeroscpf = new int[10];
+        long restante;
+        int somanumeros = 0, digito2;
+
+
+        restante = CPF_dig1;
+        for (int i = 1; i <= 10; i++)
+        {
+            numeroscpf[i - 1] = Convert.ToInt32(restante % 10);
+            restante = Convert.ToInt32(restante / 10);
+            somanumeros = somanumeros + numeroscpf[i - 1] * (i + 1);
+        }
+        digito2 = 11 - (somanumeros % 11);
+
+        if (digito2 == 2)
+        {
+            digito2 = 2;
+        }
+        else if (digito2 >= 10)
+        {
+            digito2 = 0;
+        }
+        else
+        {
+            Console.WriteLine("erro no digito");
+        }
+
+        return (digito2);
+    }
+
+    public void mensagem(int CPF_digitado)
+    {
+        int dig1 = calcdig1(CPF_digitado);
+
+        long dig2 = calcdig2(long.Parse(CPF_digitado+""+dig1));
+
+        Console.WriteLine("O seu CPF com digito é: " + CPF_digitado + "-" + dig1 + dig2);
+        Console.ReadLine();
+
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exercicio_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long CPF;
+
+            Console.WriteLine("Por favor digite o seu CPF");
+            CPF = long.Parse(Console.ReadLine());
+            if (CPF <= 1000000000 && CPF >= 99999999)
+            {
+                cad_pess_fis a = new cad_pess_fis();
+
+                a.mensagem(Convert.ToInt32(CPF));
+
+            }
+            else
+            {
+                Console.WriteLine("CPF inválido, Digite um CPF de 9 digitos!");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
